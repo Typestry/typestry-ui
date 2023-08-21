4c824a273d8a1834083dea6422a3b0634bf0f810
thispastwinter <klaas.justin@gmail.com>
2023-08-21 16:04:45 -0500

    cleaned up show container, added cta

--- src/components/ListShow/ListShow.tsx
+++ src/components/ListShow/ListShow.tsx
@@ -1,5 +1,6 @@
 import { Show } from "../../types/Show"
 import { DateUtils } from "../../utils/dateUtils/dateUtils"
+import { Button } from "../Button"
 
 interface ListShowProps {
   shows: Array<Show>
@@ -7,7 +8,7 @@ interface ListShowProps {
 
 export const ListShow = ({ shows }: ListShowProps) => {
   return (
-    <div className="bg-[rgb(51,51,51,0.8)] px-8 md:px-16 py-12 md:py-24 grid grid-flow-row gap-y-4">
+    <div className="bg-[rgb(51,51,51,0.8)] px-8 md:px-16 py-12 md:py-24 grid grid-flow-row gap-y-8">
       {shows.length ? (
         shows.map((show) => (
           <div key={show.date}>
@@ -15,10 +16,17 @@ export const ListShow = ({ shows }: ListShowProps) => {
               <p>{DateUtils.formatDate({ date: show.date })}</p>
               <p>{show.city}</p>
             </div>
-            <div className="grid grid-cols-3 pt-2">
-              <p className="mr-auto">${show.price}</p>
-              <p className="mx-auto">{show.description}</p>
-              <p className="ml-auto">{show.venue}</p>
+            <div className="grid grid-cols-1 gap-y-2 pt-2 items-center">
+              <p className="font-bold">${show.price}</p>
+              <p>{show.description}</p>
+              <p className="font-bold">{show.venue}</p>
+              <a
+                target="_blank"
+                href={show.callToAction.url}
+                aria-label={show.callToAction.altText}
+              >
+                <Button>{show.callToAction.label}</Button>
+              </a>
             </div>
           </div>
         ))
